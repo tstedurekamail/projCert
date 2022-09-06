aa10b537d0b6687c149054b788a70620da8124b1
tstedurekamail <112807815+tstedurekamail@users.noreply.github.com>
2022-09-06 22:00:02 +0530

    Update puppet_agent.sh

--- puppet_agent.sh
+++ puppet_agent.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 puppet_status=$(systemctl status puppet)
-echo $puppet_status
+
 if [[ $puppet_status =~ .*"active".* ]]
 then
-  echo -e "\nPuppet agent is already active\n"
+  echo -e "\nPuppet agent is already active and running\n"
+  echo -e "\nPuppet status: $puppet_status"
 else
   echo -e "\nInstalling and configuring puppet agent\n"
-  # sudo wget https://apt.puppetlabs.com/puppet6-release-focal.deb
-  # sudo dpkg -i puppet6-release-focal.deb
-  # sudo apt-get update -y
-  # sudo apt-get -y install puppet-agent
-  # sudo systemctl start puppet
-  # sudo systemctl enable puppet
-  # sudo systemctl status puppet
+  sudo wget https://apt.puppetlabs.com/puppet6-release-focal.deb
+  sudo dpkg -i puppet6-release-focal.deb
+  sudo apt-get update -y
+  sudo apt-get -y install puppet-agent
+  sudo systemctl start puppet
+  sudo systemctl enable puppet
+  sudo systemctl status puppet
 fi
 
